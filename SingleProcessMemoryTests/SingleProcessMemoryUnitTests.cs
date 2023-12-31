141b28cd4389afe48ee1a7b564edd7ff476bd617
bengabay1994 <bengabay1994@gmail.com>
2023-12-31 19:40:14 +0200

    updated test and versions. upgraded to .net8 on tests and migrated to netstandard2.1 on ProcessMemory project to be compatible with many different apps.

--- SingleProcessMemoryTests/SingleProcessMemoryUnitTests.cs
+++ SingleProcessMemoryTests/SingleProcessMemoryUnitTests.cs
@@ -798,7 +798,7 @@ namespace SingleProcessMemoryTests
             condToCheckTrue.Add(singleProcessMemory.WriteByteToAddress(addressOfTestedMemory + baseOffset + 24, bnumAfterUnFreeze));
             condToCheckTrue.Add(singleProcessMemory.WriteStringToAddress(addressOfTestedMemory + baseOffset + 25, snumAfterUnFreeze, encoding));
 
-            Thread.Sleep(250); // let the freeze threads write the real values back
+            Thread.Sleep(500); // let the freeze threads write the real values back
 
             int inumRead = singleProcessMemory.ReadIntFromAddress(addressOfTestedMemory + baseOffset);
             long lnumRead = singleProcessMemory.ReadLongFromAddress(addressOfTestedMemory + baseOffset + 4);
@@ -814,7 +814,7 @@ namespace SingleProcessMemoryTests
             condToCheckTrue.Add(singleProcessMemory.UnFreezeValue("key5"));
             condToCheckTrue.Add(singleProcessMemory.UnFreezeValue("key6"));
 
-            Thread.Sleep(250); // let the cancellation token enough time to cancel the Freeze Address
+            Thread.Sleep(500); // let the cancellation token enough time to cancel the Freeze Address
 
             // writing different values to make sure the Unfreeze worked.
             condToCheckTrue.Add(singleProcessMemory.WriteIntToAddress(addressOfTestedMemory + baseOffset, inumAfterUnFreeze));
